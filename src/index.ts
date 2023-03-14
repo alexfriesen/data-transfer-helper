a6ac466f6068cc88d490d044b6bb0e081a46da16
Alexander Friesen <friesen.alex.home@gmail.com>
2023-03-14 12:58:02 +0100

    ✨ add source

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { parseDataTransferItem } from "./filesystem";
+
+export async function parseDataTransferFiles(
+  list: DataTransferItemList | undefined
+) {
+  if (list) {
+    const items = Array.from(list).filter((item) => item.kind === "file");
+    const fileChunks = await Promise.all(
+      items.map(async (item) => parseDataTransferItem(item))
+    );
+    return fileChunks.flat();
+  }
+
+  return [];
+}
